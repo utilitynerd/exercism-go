818661dcaeac735731c03a69ffe6afc51a67fdb7
Mike Jones <mikejones@security.berkeley.edu>
2019-10-18 10:13:59 -0700

    make error lowercase, move distance closer to its use

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,19 +1,17 @@
 // Package hamming provides a function to calculate the hamming distance of 2 strings
 package hamming
 
-import (
-	"errors"
-)
+import "errors"
 
 // Distance returns the hamming distance of 2 strings and an optional error
 func Distance(a, b string) (int, error) {
-	// distance gets initialized as int types zero value, 0
-	var distance int
 	aRunes, bRunes := []rune(a), []rune(b)
 
 	if len(aRunes) != len(bRunes) {
-		return distance, errors.New("Strings must be the same length")
+		return 0, errors.New("strings must be the same length")
 	}
+
+	distance := 0
 	for i := range aRunes {
 		if aRunes[i] != bRunes[i] {
 			distance++
