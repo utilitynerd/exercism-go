3b464b18d9c938a7722e89542716b4054ea914d0
Mike Jones <mikejones@security.berkeley.edu>
2019-10-18 09:42:42 -0700

    finish rna download proverb

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,26 @@
 package strand
 
+import (
+	"fmt"
+	"strings"
+)
+
+var dnaToRna = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA coverts a string of DNA nucliatides to a string of RNA nucliatides
 func ToRNA(dna string) string {
+	var output strings.Builder
+	for _, c := range dna {
+		if rna, ok := dnaToRna[c]; ok {
+			output.WriteRune(rna)
+		} else {
+			panic(fmt.Sprintf("Invalid DNA nucliatides: %c", c))
+		}
+	}
+	return output.String()
 }
