131e80a0534f116f730a2eb3ae7c39c94de623e8
Mike Jones <mikejones@security.berkeley.edu>
2019-10-18 09:54:45 -0700

    Simplify hamming by converting string to slice of runes

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,26 +3,21 @@ package hamming
 
 import (
 	"errors"
-	"unicode/utf8"
 )
 
 // Distance returns the hamming distance of 2 strings and an optional error
 func Distance(a, b string) (int, error) {
 	// distance gets initialized as int types zero value, 0
 	var distance int
-	lenA, lenB := utf8.RuneCountInString(a), utf8.RuneCountInString(b)
-	if lenA != lenB {
+	aRunes, bRunes := []rune(a), []rune(b)
+
+	if len(aRunes) != len(bRunes) {
 		return distance, errors.New("Strings must be the same length")
 	}
-	for i1, i2, runes := 0, 0, 0; runes < lenA; runes++ {
-		r1, width1 := utf8.DecodeRuneInString(a[i1:])
-		r2, width2 := utf8.DecodeRuneInString(b[i2:])
-		if r1 != r2 {
+	for i := range aRunes {
+		if aRunes[i] != bRunes[i] {
 			distance++
 		}
-		i1 += width1
-		i2 += width2
 	}
-
 	return distance, nil
 }
