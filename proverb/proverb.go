3e6020ce91837b8f5426657bfd9634bd223c9f45
Mike Jones <mikejones@security.berkeley.edu>
2019-10-21 10:19:53 -0700

    solved proverb

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,23 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb supplies all your proverb needs
 package proverb
 
-// Proverb should have a comment documenting it.
+import "fmt"
+
+// Proverb constructs a deeply meaningful proverb from a slice of strings
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	inLen := len(rhyme)
+	// We can allocate a slice of exactly required length
+	output := make([]string, inLen)
+
+	for i := range rhyme {
+		if i+1 < inLen {
+			// Since proper slice is already allocated, can directly assign values
+			// instead of using append
+			output[i] = fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1])
+		}
+		if i+1 == inLen {
+			output[i] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+		}
+	}
+	return output
 }
