267ea9b70ed195568520f6f0394c670f283f9c37
Mike Jones <mikejones@security.berkeley.edu>
2019-11-01 13:04:16 -0700

    solve lugn

---
+++ luhn/luhn.go
@@ -0,0 +1,37 @@
+// Package luhn provides a function to validate strings using the Luhn algorithm
+package luhn
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Valid expects an ascii encoded string of numbers and spaces
+// and returns if the string is valid according to the Luhn algorithm
+func Valid(s string) bool {
+	s = strings.Replace(s, " ", "", -1)
+	if len(s) <= 1 {
+		return false
+	}
+	var sum int
+	var toDouble bool
+	var digit int
+	var err error
+	for i := len(s) - 1; i >= 0; i-- {
+		if digit, err = strconv.Atoi(string(s[i])); err != nil {
+			return false
+		}
+		if toDouble {
+			if digit >= 5 {
+				sum += digit*2 - 9
+			} else {
+				sum += digit * 2
+			}
+			toDouble = false
+		} else {
+			sum += digit
+			toDouble = true
+		}
+	}
+	return sum%10 == 0
+}
