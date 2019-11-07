912fe9391dacc70166e360645241a4494d38fbaf
Mike Jones <mikejones@security.berkeley.edu>
2019-11-06 16:46:17 -0800

    solve grains

---
+++ grains/grains.go
@@ -0,0 +1,22 @@
+// Package grains provides functions to help solve the classic wheat and chessboard problem
+// https://en.wikipedia.org/wiki/Wheat_and_chessboard_problem
+package grains
+
+import (
+	"fmt"
+	"math"
+)
+
+// Square retruns the number of grains on a specific chessboard square
+func Square(i int) (grains uint64, err error) {
+	if i <= 0 || i > 64 {
+		return 0, fmt.Errorf("invalid square %d", i)
+	}
+	grains = uint64(math.Exp2(float64(i - 1)))
+	return grains, nil
+}
+
+// Total returns the total number of wheat grains on a filled chessboard
+func Total() (grains uint64) {
+	return math.MaxUint64
+}
