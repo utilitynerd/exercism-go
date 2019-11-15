da6680df7d6d847a575d4ce57ce80df9b38d1354
Mike Jones <mikejones@security.berkeley.edu>
2019-11-14 16:07:26 -0800

    finish sieve

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,13 +1,30 @@
 package sieve
 
-func Sieve(limit int) []int {
-	n := make(map[int]struct{})
-	var s struct{}
-	for i := 2; i <= limit+1; i++ {
-		n[i] = s
-	}
-	for i = 2; i <= limit+1; i++ {
+import "sort"
 
+// Sieve implements the sieve of eratosthenes to return an []int of all primes up to limit
+func Sieve(limit int) []int {
+	if limit < 2 {
+		return make([]int, 0)
 	}
-	return make([]int, 0)
+	candidates := make(map[int]struct{})
+	var s struct{}
+	for i := 2; i <= limit; i++ {
+		candidates[i] = s
+	}
+
+	for i := 2; i <= limit; i++ {
+		for j := i * 2; j <= limit; j += i {
+			delete(candidates, j)
+		}
+	}
+
+	primes := make([]int, len(candidates))
+	i := 0
+	for k := range candidates {
+		primes[i] = k
+		i++
+	}
+	sort.Ints(primes)
+	return primes
 }
