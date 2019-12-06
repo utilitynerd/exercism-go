8e2ce5b7ae09fe253304b101188a0f4f48714313
Mike Jones <mikejones@security.berkeley.edu>
2019-12-05 16:42:50 -0800

    refactor tree building

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -19,32 +19,33 @@ type Node struct {
 
 // Build builds a tree of Nodes from a slice of records
 func Build(records []Record) (*Node, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
 	// Sorting the input records ensures a parent record
 	// is handled before any of its children
 	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 
-	root := records[0]
-	if root.ID != 0 || root.Parent != 0 {
-		return nil, errors.New("invalid root node")
-	}
 	nodes := make(map[int]*Node)
-	tree := &Node{ID: 0}
-	nodes[0] = tree
-	for i, r := range records[1:] {
-		if i+1 != r.ID {
-			return nil, errors.New("non continuous")
-		}
-		if r.Parent >= r.ID {
-			return nil, errors.New("invalid parent")
-		}
-		n := &Node{ID: r.ID, Children: nil}
-		nodes[r.ID] = n
+	var tree *Node
+	for i, r := range records {
+		if i == 0 {
+			if r.ID != 0 || r.Parent != 0 {
+				return nil, errors.New("invalid root node")
+			}
+			tree = &Node{ID: 0}
+			nodes[0] = tree
+		} else {
+			if i != r.ID {
+				return nil, errors.New("non continuous")
+			}
+			if r.Parent > 0 && r.Parent >= r.ID {
+				return nil, errors.New("invalid parent")
+			}
+			n := &Node{ID: r.ID, Children: nil}
+			nodes[r.ID] = n
 
-		parent := nodes[r.Parent]
-		parent.Children = append(parent.Children, n)
+			parent := nodes[r.Parent]
+			parent.Children = append(parent.Children, n)
+		}
+
 	}
 	return tree, nil
 }
