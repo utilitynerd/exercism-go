7a7402ebdcbd7576f49b4ed18a1285739f3a390e
Mike Jones <mikejones@security.berkeley.edu>
2019-12-12 16:53:08 -0800

    refactor tree_building

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -24,28 +24,15 @@ func Build(records []Record) (*Node, error) {
 	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 
 	nodes := make(map[int]*Node)
-	var tree *Node
 	for i, r := range records {
-		if i == 0 {
-			if r.ID != 0 || r.Parent != 0 {
-				return nil, errors.New("invalid root node")
-			}
-			tree = &Node{ID: 0}
-			nodes[0] = tree
-		} else {
-			if i != r.ID {
-				return nil, errors.New("non continuous")
-			}
-			if r.Parent > 0 && r.Parent >= r.ID {
-				return nil, errors.New("invalid parent")
-			}
-			n := &Node{ID: r.ID, Children: nil}
-			nodes[r.ID] = n
-
-			parent := nodes[r.Parent]
-			parent.Children = append(parent.Children, n)
+		if (r.ID == 0 && r.Parent != 0) || i != r.ID || (r.ID > 0 && r.Parent >= r.ID) {
+			return nil, errors.New("non sequential or invalid parent")
 		}
-
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID > 0 {
+			p := nodes[r.Parent]
+			p.Children = append(p.Children, nodes[r.ID])
+		}
 	}
-	return tree, nil
+	return nodes[0], nil
 }
